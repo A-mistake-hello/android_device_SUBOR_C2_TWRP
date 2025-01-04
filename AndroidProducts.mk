ef86e55efc5422a5968dd4d4f274843a20fadd3e
A-mistake-hello <3329989998@qq.com>
2025-01-04 14:44:23 +0800

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_s20_xbw_gl.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_s20_xbw_gl-user \
+    omni_s20_xbw_gl-userdebug \
+    omni_s20_xbw_gl-eng

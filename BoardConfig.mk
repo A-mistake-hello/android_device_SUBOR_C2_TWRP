95bede6bab8397dd51bc095767abea94b0a66018
A-mistake-hello <3329989998@qq.com>
2025-05-24 19:53:42 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -73,6 +73,7 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+TW_INCLUDE_CRYPTO := true
 
 # TWRP Version
 TW_DEVICE_VERSION := $(shell date -u +"%Y%m%d"-"A-Mistake")
